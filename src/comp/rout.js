ce75ab3f28a786572789d9d658bcf93bb3a07ea2
Aryan Jamatia <aryanjamatia9862@gmail.com>
2024-10-04 10:45:07 +0530

    Updated files with recent changes

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Change this to react-router-dom
+import { Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Shop from './shop';
 import Cart from './cart';
