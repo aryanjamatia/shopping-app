f1db6c646b867327d79b8c8b23f3342aee92f177
Aryan Jamatia <aryanjamatia9862@gmail.com>
2024-10-04 11:05:11 +0530

    Updated files with recent changes

--- src/comp/home_product.js
+++ src/comp/home_product.js
@@ -1,3 +1,15 @@
+import p1 from './image/p1.webp';
+import p2 from './image/p2.webp';
+import p3 from './image/p3.webp';
+import p4 from './image/p9.jpg';
+import p5 from './image/p4.webp';
+import p6 from './image/p5.webp';
+import p7 from './image/p10.jpg';
+import p8 from './image/p11.jpg';
+import p9 from './image/p6.webp';
+import p10 from './image/p7.webp';
+import p11 from './image/p8.webp';
+import p12 from './image/p12.jpg';
 const Homeproduct = [
     {
         id: 1,
